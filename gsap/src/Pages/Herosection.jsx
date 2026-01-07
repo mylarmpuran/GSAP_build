27c73ddc45ee0e30a3409563c6781f9cb6b9ac3f
mylarmpuran <mylarmpuran@gmail.com>
2026-01-07 20:47:11 +0530

    "add on"

---
+++ gsap/src/Pages/Herosection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Herosection() {
+  return (
+      <h1 className="text-3xl font-bold text-red-500 underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default Herosection;
