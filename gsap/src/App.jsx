52395136fcf7fb616f69f550c403acfea18de0fe
mylarmpuran <mylarmpuran@gmail.com>
2026-01-08 01:00:43 +0530

    'method as props"

--- gsap/src/App.jsx
+++ gsap/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 
 import Herosection from './Pages/Herosection'
 import Hello from './Components/Hello'
+import Welcome from './Components/Welcome.jsx'
+import Message from './Components/Message.jsx'
+import Counter from './Components/Counter.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +14,9 @@ function App() {
     <>
      <Herosection/>
      <Hello/>
+     <Welcome name="puran" heroName="mylaram"/>
+     <Message/>
+     <Counter/>
 
     </>
   )
