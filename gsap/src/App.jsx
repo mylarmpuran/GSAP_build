a56f2423c492a82bf345e4e93b9372e270db4cc7
mylarmpuran <mylarmpuran@gmail.com>
2026-01-07 22:29:35 +0530

    "jsx"

--- gsap/src/App.jsx
+++ gsap/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import Herosection from './Pages/Herosection'
+import Hello from './Components/Hello'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
      <Herosection/>
+     <Hello/>
+
     </>
   )
 }
---
+++ gsap/src/Components/Hello.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = () => {
+    return (
+        <div>
+            <h1 className="text-red-600">
+                Hello Vishwas
+            </h1>
+        </div>
+    )
+}
+
+export default Hello;
